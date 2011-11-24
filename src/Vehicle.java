87750b293dab621ea62afe812dc59b00f945ff03
Kewin Remeczki <fckewin@hotmail.com>
2011-11-24 13:34:23 +0100

    Alle bil-subklasser oprettet

---
+++ src/Vehicle.java
@@ -0,0 +1,29 @@
+/**
+ * @author Anders
+ *
+ */
+public class Vehicle {
+
+protected int carID;
+protected String carModel;
+protected String description;
+
+/**
+ * The getCarID method
+ * returns the ID of the car.
+ */
+public int getCarID()
+{
+	return carID;
+}
+
+/**
+ * the getCarModel method
+ * returns the carModel
+ */
+public String getCarModel()
+{
+	return carModel;
+}
+		
+}
